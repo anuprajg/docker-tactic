1fc00452bee4cce0e5a344dd64c2205719c6de43
anuprajg <anuprajg@users.noreply.github.com>
2018-03-02 12:23:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 TIMESTAMP=`date +%Y-%m-%d`
 alias docker='sudo docker'
 sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
-docker build -t diegocortassa/tactic .
-docker build -t diegocortassa/tactic:4.5.0.v01 .
-docker push diegocortassa/tactic
+docker build -t anuprajg/tactic .
+docker push anuprajg/tactic
